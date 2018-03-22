b1d52f6f7251f102190f2e5460986fa26ff71cc9
Walker Schmidt <schmidt.walker@yahoo.com>
2018-03-21 21:57:10 -0600

    Added Administrator and Trader class

---
+++ PaperTrader/TraderApp/User/Administrator.py
@@ -0,0 +1,17 @@
+
+class Administrator:
+
+	def __init__(self, adminId):
+		self.__adminId = adminId
+
+	def getAdminId(self):
+		return self.__adminId
+
+	def addStock(self, symb):
+		pass
+
+	def getUserByName(self, name):
+		pass
+
+	def getUserById(self, id):
+		pass
---
+++ PaperTrader/TraderApp/User/Trader.py
@@ -0,0 +1,8 @@
+
+class Trader:
+
+	def __init__(self, portfolio):
+		self.__portfolio = portfolio
+
+	def getPortfolio(self):
+		return self.__portfolio
