0830eb1cd4af988f5531a6aa1e2967353ed19b3c
Dante Pasionek <dante.pasionek1@gmail.com>
2018-04-06 11:43:45 -0600

    Created skeleton for that portfolio nonsense!

--- PaperTrader/PaperTraderApp/admin.py
+++ PaperTrader/PaperTraderApp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from PaperTraderApp.models import StockModel
+
+admin.site.register(StockModel)
 
 # Register your models here.
+
+
+
+
--- PaperTrader/PaperTraderApp/models.py
+++ PaperTrader/PaperTraderApp/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.urls import reverse
+import json
 
 # Create your models here.
 MAX_LENGTH_NAME = 60
 MAX_LENGTH_SYMB = 6
 MAX_DIGITS = 20
+
 
 class StockModel(models.Model):
     name = models.CharField(max_length=MAX_LENGTH_NAME)
@@ -20,3 +22,17 @@ class StockModel(models.Model):
 
     def get_absolute_url(self):
         return reverse("stock-list", kwargs={ 'pk' : self.pk }) # We'll need this when it comes to deleting stock object
+
+class AdminStockModel(models.Model):
+    name = models.CharField(max_length=MAX_LENGTH_NAME)
+
+class PortfolioModel(models.Model):
+    stocks = models.CharField(max_length=99999999999)
+    balance = models.FloatField()
+
+    def set_stocks(stock_list, self):
+        self.stocks = json.dumps(stock_list)
+    def get_stocks(self):
+        return json.loads(self.stocks)
+    
+
